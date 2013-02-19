e4c06e31604840c2f47323548480de160af16e10
Matt Thomas <matt@betweenbrain.com>
2013-02-19 11:57:10 -0500

    Improved diagnostics

--- elements/diagnostic.php
+++ elements/diagnostic.php
@@ -2,7 +2,7 @@
 
 /**
  * File       diagnostic.php
- * Created    12/31/12 11:59 AM
+ * Created    2/12/13 11:59 AM
  * Author     Matt Thomas
  * Website    http://betweenbrain.com
  * Email      matt@betweenbrain.com
@@ -13,78 +13,82 @@
 
 class JElementDiagnostic extends JElement {
 
-    function fetchElement() {
+	function fetchElement() {
 
-        // Fetch parameters via database query
-        $db  = JFactory::getDBO();
-        $sql = "SELECT params
+		// Fetch parameters via database query
+		$db  = JFactory::getDBO();
+		$sql = 'SELECT params
           FROM #__modules
-          WHERE module = \"mod_skeleton\"
-          /*AND published = 1*/";
-        $db->setQuery($sql);
-        $params = $db->loadResult();
+          WHERE module = "mod_skeleton"';
+		$db->setQuery($sql);
+		$params = $db->loadResult();
 
-        // In Joomla 1.5, parameters are stored as a string, so we need to match condition with strpos.
-        $displaydiagnostic = strpos($params, 'displaydiagnostic=1');
-        $cache             = strpos($params, 'cache=1');
+		// In Joomla 1.5, parameters are stored as a string, so we need to match condition with strpos.
+		$displaydiagnostic = strpos($params, 'displaydiagnostic=1');
+		$cache             = strpos($params, 'cache=1');
 
-        // JPATH_CACHE is relative to where it is being called from, as we want the site cache, /administrator is removed.
-        $cachedir = JPATH_CACHE . '/mod_skeleton/';
-        $cachedir = preg_replace("/administrator\//", '', $cachedir);
-        // Determine cache maximum age as set by parameter
-        $cachemaxage = preg_match("/cachemaxage=[(0-9)*]/", $params);
+		// JPATH_CACHE is relative to where it is being called from, as we want the site cache, /administrator is removed.
+		$cachedir = JPATH_CACHE . '/mod_skeleton/';
+		$cachedir = preg_replace("/administrator\//", '', $cachedir);
+		// Determine cache maximum age as set by parameter
+		$cachemaxage = preg_match("/cachemaxage=[(0-9)*]/", $params);
 
-        // Initialize variables
-        $result   = NULL;
-        $messages = NULL;
-        $errors   = NULL;
+		// Initialize variables
+		$result   = NULL;
+		$messages = NULL;
+		$errors   = NULL;
 
-        if ($displaydiagnostic) {
+		if ($displaydiagnostic) {
 
-            // Check cache stuff
-            if (!$cache) {
-                $messages[] = "Caching is disabled.";
-            }
+			// Check cache stuff
+			if (!$cache) {
+				$messages[] = "Caching is disabled.";
+			}
 
-            if ($cache) {
+			if ($cache) {
 
-                $messages[] = "Caching is enabled.";
+				$messages[] = "Caching is enabled.";
 
-                $messages[] = "Cache lifetime is $cachemaxage minute(s).<br/>";
+				$cache    = $cachedir . 'cache.json';
+				$cacheAge = date("F d Y H:i:s", filemtime($cache));
 
-                if (is_dir($cachedir)) {
-                    $messages[] = "Cache dirtectory exists at $cachedir";
-                } else {
-                    $errors[] = "The cache diretory at $cachedir does not exist!";
-                }
+				if (file_exists($cache)) {
+					$messages[] = "The cache file exists at $cache.";
+					$messages[] = "The cache file was created $cacheAge.";
+				}
 
-            }
+				$messages[] = "Cache lifetime is $cachemaxage minute(s).<br/>";
 
-            if ($messages[0]) {
-                $result .= '<dl id="system-message"><dt>Information</dt><dd class="message fade"><ul>';
-                foreach ($messages as $message) {
-                    $result .= '<li>' . $message . '</li>';
-                }
-                $result .= '</ul></dd></dl>';
-            }
+				if (is_dir($cachedir)) {
+					$messages[] = "Cache dirtectory exists at $cachedir.";
+				} else {
+					$errors[] = "The cache diretory at $cachedir does not exist!";
+				}
+			}
 
-            if ($errors[0]) {
-                $result .= '<dl id="system-message"><dt>Errors</dt><dd class="error message fade"><ul>';
-                foreach ($errors as $error) {
-                    $result .= '<li>' . $error . '</li>';
-                }
-                $result .= '</ul></dd></dl>';
-            }
+			if ($messages[0]) {
+				$result .= '<dl id="system-message"><dt>Information</dt><dd class="message fade"><ul>';
+				foreach ($messages as $message) {
+					$result .= '<li>' . $message . '</li>';
+				}
+				$result .= '</ul></dd></dl>';
+			}
 
-            if ($result) {
-                return print_r($result, FALSE);
-            }
+			if ($errors[0]) {
+				$result .= '<dl id="system-message"><dt>Errors</dt><dd class="error message fade"><ul>';
+				foreach ($errors as $error) {
+					$result .= '<li>' . $error . '</li>';
+				}
+				$result .= '</ul></dd></dl>';
+			}
 
-            return FALSE;
+			if ($result) {
+				return print_r($result, FALSE);
+			}
 
-        }
+			return FALSE;
+		}
 
-        return FALSE;
-    }
-
+		return FALSE;
+	}
 }
